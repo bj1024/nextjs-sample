66a724df573080d26cd1f75b11586a4a373dadb5
H.K <16552133+bj1024@users.noreply.github.com>
2021-10-19 11:38:21 +0900

    remove signin.css.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,6 @@ import type { AppProps } from 'next/app'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/globals.scss'
-// import '../styles/signin.css'
 // import Head from 'next/head'
 // import Layout from '../components/layout'
 
